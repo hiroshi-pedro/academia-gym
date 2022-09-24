382c86a4e09c5aaa86e6fd694700da8bfe4456e0
Pedro Hiroshi Yoshida Dos Santos <98420552+hiroshi-pedro@users.noreply.github.com>
2022-09-23 23:05:26 -0300

    20

--- links-principais/slider-fotos.js
+++ links-principais/slider-fotos.js
@@ -2,6 +2,8 @@ $(function(){
   $(".your-class").slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,
+    prevArrow: $('#prev-l-l'),
+    nextArrow: $('#prev-l-h')
   })
 })
