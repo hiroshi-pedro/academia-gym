630efc965c4086898858960bc8afe4b2a85fda49
Pedro Hiroshi Yoshida Dos Santos <98420552+hiroshi-pedro@users.noreply.github.com>
2022-10-01 19:58:52 -0300

    bhjjh

--- slider-f.js
+++ slider-f.js
@@ -1,7 +1,7 @@
 $(function(){
     $('.containerproje').slick({
         autoplay: true,
-        autoplaySpeed: 7000000,//O tempo será de 8 segundos mas por enquanto está 1h
+        autoplaySpeed: 70000000,//O tempo será de 8 segundos mas por enquanto está 1h
         prevArrow: $('#prev'),
         nextArrow: $('#next')
     });
