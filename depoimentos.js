6412803477e30128e7dc597fd40b4a97343be4af
Pedro Hiroshi Yoshida Dos Santos <98420552+hiroshi-pedro@users.noreply.github.com>
2022-12-19 00:24:38 -0300

    adapitação

--- depoimentos.js
+++ depoimentos.js
@@ -5,25 +5,25 @@ const testimonials = [
     name: "Pedro Hiroshi",
     job: "aluno",
     testimonial:
-      "Uma academia para todos os tipos de pessoas, o Melhor atendimento, e boa qualidade dos aparelhos. Na academia Themis treinar se torna a melhor hora do dia ;)",
+      "Uma academia para todos os tipos de pessoas, o Melhor atendimento, e boa qualidade dos aparelhos. Na academia treinar se torna a melhor hora do dia ;)",
   },
   {
     name: "Gilherme Henrique",
     job: "Aluno",
     testimonial:
-      "Academia muito boa, ambiente bom me sinto até feliz treinando lá agora que sofreu bastante reformas e mudanças no interior e exterior da academia, recomendo muito!",
+      "Academia muito boa, ambiente bom me sinto até feliz treinando lá, agora que sofreu bastante reformas e mudanças no interior e exterior da academia, recomendo muito!",
   },
   {
     name: "Kauã da Silva Virgilio",
     job: "Aluno",
     testimonial:
-      "Academia muito boa, recebeu uma reforma que mudou o jeito que eu olhava pra ela e ficou muito bonito, e as máquinas são muito boas também, 10/10 para a academia e as pessoas que cuidaram dela",
+      "Academia muito boa, recebeu uma reforma que mudou o jeito que eu olhava pra ela, e ficou muito bonita, e as máquinas são muito boas também, 10/10 para a academia e as pessoas que cuidaram dela",
   },
   {
-    name: "Nicola Blakely",
-    job: "CEO,Zeal Wheels",
+    name: "maria da silva santos",
+    job: "aluna",
     testimonial:
-      "Sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit",
+      "Gosto muito dessa academia, me sinto respeitada. aparelhos bons e uma otima professora.",
   },
 ];
 
--- links-principais/slider-fotos.js
+++ links-principais/slider-fotos.js
@@ -28,10 +28,29 @@ $(function(){
   });
 
   $(".your-class2").slick({
-    infinite: true,
-    slidesToShow: 2,
-    slidesToScroll: 2,
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerPadding: '25px',
+          slidesToShow: 1
+        }
+      }
+    ],
     prevArrow: $('#prev-l-l2'),
     nextArrow: $('#prev-l-h2')
-  })
+  });
 })
